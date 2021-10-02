ec899fa840adecfb7c0c9de7dc22259b4bce06fe
Derex Wangmang <dwangmang@gmail.com>
2021-10-02 13:40:20 -0500

    Modularized code

---
+++ src/components/CourseList.js
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { getCourseTerm, hasConflict, getCourseNumber, terms } from '../utilities/times.js';
+
+const CourseList = ({ courses }) => {
+    const [term, setTerm] = useState('Fall');
+    const [selected, setSelected] = useState([]);
+    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+    
+    return (
+      <>
+        <TermSelector term={term} setTerm={setTerm} />
+        <div className="course-list">
+        { 
+          termCourses.map(course =>
+            <Course key={ course.id } course={ course }
+              selected={selected} setSelected={ setSelected } 
+            />) 
+        }
+        </div>
+      </>
+    );
+};
+
+const TermSelector = ({term, setTerm}) => (
+    <div className="btn-group">
+    { 
+      Object.values(terms).map(value => (
+        <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+      ))
+    }
+    </div>
+);
+
+const TermButton = ({term, setTerm, checked}) => (
+    <>
+      <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+        onChange={() => setTerm(term)} />
+      <label class="btn btn-success m-1 p-2" htmlFor={term}>
+      { term }
+      </label>
+    </>
+);
+
+const toggle = (x, lst) => (
+    lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
+
+export const Course = ({ course, selected, setSelected }) => {
+    const isSelected = selected.includes(course);
+    const isDisabled = !isSelected && hasConflict(course, selected);
+    const style = {
+      backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
+    };
+    return (
+      <div className="card m-1 p-2" 
+        style={style}
+        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
+        <h6 className="card-header text-center">{ getCourseTerm(course) } CS { getCourseNumber(course) }</h6>
+        <div className="card-body">
+          <div className="card-text">{ course.title }</div>
+        </div>
+      </div>
+    );
+};
+
+export default CourseList;
